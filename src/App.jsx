761dc918241d22ba96e9541cb14f77f0f9da0ac6
Aaron Sanchez <saarong12@gmail.com>
2024-02-19 16:49:49 -0600

    Added initial login functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import axios from 'axios'
 import './App.css'
 
-function App() {
+const App = () => {
+  const onClick = async () => {
+    const res = await axios.get('http://localhost:3000/users')
+    console.log(res)
+  }
   return (
     <>
       <h1>Project Hermes</h1>
       <h2> Find your duo </h2>
+      <button>
+        <a href="http://localhost:3000/login">Login</a>
+      </button>
+      <button onClick={onClick}>Get Users</button>
     </>
   )
 }
