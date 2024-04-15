6996b2aa4a1025ad1c0e704634642e21b314d146
Aaron Sanchez <saarong12@gmail.com>
2024-04-15 14:36:04 -0500

    Added test call to hit the backend.
    Setup front end auth and created login and logout buttons.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
 import { BrowserRouter } from 'react-router-dom'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Auth0Provider
+    domain="dev-wfrnzblw5rylyqxk.us.auth0.com"
+    clientId="HZ3nBT1D3gZsller6vHzO0k4K9NS73UX"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+      audience: 'http://localhost:3000/'
+    }}
+  >
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Auth0Provider>
 )
