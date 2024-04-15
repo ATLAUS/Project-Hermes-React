16a35d7ec78e11664d5c9331ad7a63cb68502570
Daniel Guerrero <danxguerrero@google.com>
2024-04-15 10:25:20 -0400

    Set up proxy and can get data from back end

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,8 +1,26 @@
 import './Landing.scss'
+import { useEffect, useState } from 'react';
+
 export const Landing = () => {
+
+  const [user, setUser] = useState({})
+
+  const fetchUserData = async () => {
+    const response = await fetch('/api/users')
+    const userData = await response.json()
+    setUser(userData.user)
+  }
+
+  useEffect(() => {
+    fetchUserData();
+  }, [])
+
   return (
     <>
       <p className="landing">Landing Page!</p>
+      <button><a href="http://localhost:3000/">Login</a></button>
+      <button onClick={fetchUserData}>Click</button>
+      {user && <p>{user.userName}</p>}
     </>
   )
 }
