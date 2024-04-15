ee0e1d14fb64a3262e85c3972b663e038374450b
axelaquino3 <axelaquino@google.com>
2024-04-15 18:43:26 -0400

    Created Messenger component

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,9 +1,11 @@
 import './Dashboard.scss'
+import { Messenger } from  './components/messanger/Messenger'
 
 export const Dashboard = () => {
     return (
         <>
             <p className='dashboard'>Dashboard Page!!</p>
+            <Messenger />
         </>
     )
     
---
+++ src/pages/dashboard/components/messanger/Messenger.jsx
@@ -0,0 +1,10 @@
+import './Messenger.scss'
+
+
+export const Messenger = () => {
+    return(
+        <>
+            <p className='messenger'>Messenger goes here!!!</p>
+        </>
+    )
+}
