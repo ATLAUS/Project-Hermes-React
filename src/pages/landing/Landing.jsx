9b14a0cd4de42f0a9e073797a72c79dc5105ad96
axelaquino3 <axelaquino@google.com>
2024-04-26 18:00:22 -0400

    Completed layout for landing page

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -51,7 +51,7 @@ export const Landing = () => {
           <div className="nav-container">
             <img
               className="logo"
-              src="src/pages/landing/stock video images/sean-stone-dkb3wApu5XQ-unsplash.jpeg"
+              src="https://img.freepik.com/free-vector/video-game-controls-neon-style-brick-wall_24908-58916.jpg?w=740&t=st=1714157158~exp=1714157758~hmac=ba3c7319cd3c474dac0ed7d93b143d78b6db633459121d5e450867937ae0d352"
               alt="Controller"
             />
             <LoginButton />
@@ -61,7 +61,43 @@ export const Landing = () => {
             <p>Description</p>
           </div>
         </div>
-        <div className="details-1"></div>
+        <div className="details">
+          <img
+            className="details-img"
+            src="https://img.freepik.com/free-vector/video-game-controls-neon-style-brick-wall_24908-58916.jpg?w=740&t=st=1714157158~exp=1714157758~hmac=ba3c7319cd3c474dac0ed7d93b143d78b6db633459121d5e450867937ae0d352"
+          ></img>
+          <div>
+            <h1>Feature Title</h1>
+            <p>Feature Description</p>
+          </div>
+        </div>
+        <div className="details">
+          <div>
+            <h1>Feature Title</h1>
+            <p className="feature-desc">Feature Description</p>
+          </div>
+          <img
+            className="details-img"
+            src="https://img.freepik.com/free-vector/video-game-controls-neon-style-brick-wall_24908-58916.jpg?w=740&t=st=1714157158~exp=1714157758~hmac=ba3c7319cd3c474dac0ed7d93b143d78b6db633459121d5e450867937ae0d352"
+          ></img>
+        </div>
+        <div className="details3">
+          <img
+            className="details3-img"
+            src="https://img.freepik.com/free-vector/video-game-controls-neon-style-brick-wall_24908-58916.jpg?w=740&t=st=1714157158~exp=1714157758~hmac=ba3c7319cd3c474dac0ed7d93b143d78b6db633459121d5e450867937ae0d352"
+          ></img>
+          <div className="details3-button">
+            <p>Call To Action</p>
+            <LoginButton />
+          </div>
+        </div>
+        <footer>
+          <div className="footer-container">
+            <p>Github</p>
+            <p>Twitter</p>
+            <p>Instagram</p>
+          </div>
+        </footer>
       </section>
     </>
   );
