a438fc87f3466afc0af45c48a6769cff81b28982
Aaron Sanchez <saarong12@gmail.com>
2024-05-02 23:21:17 -0500

    Added service implementation.
    Need to move function to the service file to clean up the component.

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import * as pages from './pages'
 import * as components from './components'
 import { useAuth0 } from '@auth0/auth0-react'
 import { Route, Routes } from 'react-router-dom'
+import { useState } from 'react'
 
 const App = () => {
+  const [dbUser, setDbUser] = useState(null)
   const { isLoading } = useAuth0()
 
   if (isLoading) {
@@ -20,7 +22,7 @@ const App = () => {
     <main>
       <Routes>
         <Route path="/" element={<pages.Landing />}></Route>
-        {/* TODO If login page is not completed replace with unauth page
+        {/* TODO If login page is not completed replace with 404 page
         OR condense the two. */}
         <Route path="/login" element={<pages.LoginPage />}></Route>
         <Route
@@ -31,7 +33,7 @@ const App = () => {
           path="/dashboard"
           element={
             <components.ProtectedRoute>
-              <pages.Dashboard />
+              <pages.Dashboard dbUser={dbUser} setDbUser={setDbUser} />
             </components.ProtectedRoute>
           }
         ></Route>
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,21 +1,75 @@
-import { Box, Grid, Typography } from '@mui/material'
+// Removed Typography import to get rid of linter error.
+import { Box, Grid } from '@mui/material'
 import './Dashboard.scss'
-import { Messenger } from  './components/messenger/Messenger'
+import { Messenger } from './components/messenger/Messenger'
 import { GameInfo } from './components/game-info/GameInfo'
 import { PartyInfo } from './components/match-info/PartyInfo'
+import { useEffect } from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
 
-export const Dashboard = () => {
-    return (
-        <Grid container spacing={3} className='dashboard-container' sx={{height:'100%', justifyContent: 'center', alignItems: 'center'}}>
-            <Grid item xs={3} sx={{height: '80%'}}>
-                <GameInfo />
-                <PartyInfo />
-            </Grid>
-            <Grid item xs={6} sx={{height: '80%'}}>
-                <Box sx={{border: 'solid red 1px', borderRadius: '5px', height: '100%'}}>
-                    <Messenger/>
-                </Box>
-            </Grid>
-        </Grid>
-    )
+export const Dashboard = ({ dbUser, setDbUser }) => {
+  const { user, getAccessTokenSilently } = useAuth0()
+
+  // TODO Evaluate how to move to service file.
+  const fetchUser = async () => {
+    try {
+      const accessToken = await getAccessTokenSilently({
+        authorizationParams: {
+          audience: 'http://localhost:3000/'
+        }
+      })
+
+      const customUserHeader = {
+        'X-User-Info': JSON.stringify({
+          email: user.email,
+          nickname: user.nickname,
+          sub: user.sub
+        })
+      }
+
+      const getUser = 'http://localhost:3000/api/users'
+
+      const getUserResponse = await fetch(getUser, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+          ...customUserHeader
+        }
+      })
+
+      const response = await getUserResponse.json()
+
+      console.log(response)
+      setDbUser(response)
+    } catch (e) {
+      console.log(e.message)
+    }
+  }
+
+  useEffect(() => {
+    fetchUser()
+  }, [])
+
+  console.log(dbUser)
+
+  return (
+    <Grid
+      container
+      spacing={3}
+      className="dashboard-container"
+      sx={{ height: '100%', justifyContent: 'center', alignItems: 'center' }}
+    >
+      <Grid item xs={3} sx={{ height: '80%' }}>
+        <GameInfo />
+        <PartyInfo />
+      </Grid>
+      <Grid item xs={6} sx={{ height: '80%' }}>
+        <Box
+          sx={{ border: 'solid red 1px', borderRadius: '5px', height: '100%' }}
+        >
+          <Messenger />
+        </Box>
+      </Grid>
+    </Grid>
+  )
 }
