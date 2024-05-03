992e3bfcec6187b799688598e6c908a9c8eec5dc
ghezaii <ghezai@google.com>
2024-05-03 14:18:37 -0400

    Set up form data

--- src/pages/matcher-display/components/matcher-form/MatcherForm.jsx
+++ src/pages/matcher-display/components/matcher-form/MatcherForm.jsx
@@ -1,5 +1,6 @@
 import { Box, Typography } from '@mui/material'
-import { Stack, TextField, Button, MenuItem } from '@mui/material'
+import { Stack, TextField, Button, MenuItem, FormControl } from '@mui/material'
+import { useState } from 'react'
 import './MatcherForm.scss'
 
 const platforms = [
@@ -10,55 +11,90 @@ const platforms = [
 ];
 
 export const MatcherForm = () => {
+    const [game, setGame] = useState("")
+    const [platform, setPlatform] = useState("")
+    const [objective, setObjective] = useState("")
+    const [note, setNote] = useState("")
+
+    async function submitHandler(e) {
+        e.preventDefault();
+        const newMatcher = {
+            game,
+            platform,
+            objective,
+            note
+        }
+    
+    try {
+        await fetch(`localhost:3000/api/matchers`, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(newMatcher)
+        })
+    } catch(err) {
+        console.log("error", err)
+    }
+    }
+
     return(
         <Box className = "matcher-form-container">
             <h2>Project Hermes Matcher Form</h2>
             <h3>Please reach out to us if you have any questions!</h3>
-            <Stack spacing={2}>
-                <TextField
-                    type = "text"
-                    variant = "outlined"
-                    label = "Game"
-                    InputProps={{style: {color: "white"}}}
-                    fullWidth
-                    required
-                    focused
-                />
-                <TextField
-                    type = "text"
-                    variant = "outlined"
-                    label = "Platform"
-                    InputProps={{style: {color: "white"}}}
-                    select
-                    fullWidth
-                    required
-                    focused
-                >
-                    {platforms.map((option) => (
-                        <MenuItem key={option.value} value={option.value}>
-                            {option.label}
-                        </MenuItem>
-                    ))}
-                </TextField>
-                <TextField
-                    type = "text"
-                    variant = "outlined"
-                    label = "Objective"
-                    InputProps={{style: {color: "white"}}}
-                    fullWidth
-                    focused
-                />
-                <TextField
-                    type = "text"
-                    variant = "outlined"
-                    label = "Note"
-                    InputProps={{style: {color: "white"}}}
-                    multiline
-                    fullWidth
-                    focused
-                />
-                <Button variant = "text">Submit</Button>
-            </Stack>
+            <FormControl onSubmit={submitHandler}>
+                <Stack spacing={2}>
+                    <TextField
+                        type = "text"
+                        variant = "outlined"
+                        label = "Game"
+                        InputProps={{style: {color: "white"}}}
+                        fullWidth
+                        required
+                        focused
+                        value = {game}
+                        onChange={(e) => setGame(e.target.value)}
+                    />
+                    <TextField
+                        type = "text"
+                        variant = "outlined"
+                        label = "Platform"
+                        InputProps={{style: {color: "white"}}}
+                        select
+                        fullWidth
+                        required
+                        focused
+                        value = {platform}
+                        onChange={(e) => setPlatform(e.target.value)}
+                    >
+                        {platforms.map((option) => (
+                            <MenuItem key={option.value} value={option.value}>
+                                {option.label}
+                            </MenuItem>
+                        ))}
+                    </TextField>
+                    <TextField
+                        type = "text"
+                        variant = "outlined"
+                        label = "Objective"
+                        InputProps={{style: {color: "white"}}}
+                        fullWidth
+                        focused
+                        value = {objective}
+                        onChange={(e) => setObjective(e.target.value)}
+                    />
+                    <TextField
+                        type = "text"
+                        variant = "outlined"
+                        label = "Note"
+                        InputProps={{style: {color: "white"}}}
+                        multiline
+                        fullWidth
+                        focused
+                        value = {note}
+                        onChange={(e) => setNote(e.target.value)}
+                    />
+                    <Button type = "submit" variant = "text">Submit</Button>
+                </Stack>
+            </FormControl>
         </Box>
     )
 }
