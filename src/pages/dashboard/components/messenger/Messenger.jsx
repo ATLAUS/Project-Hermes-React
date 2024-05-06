cab9ff6feba8c885edd18c67f5ba1e63e799f3c2
Aaron Sanchez <saarong12@gmail.com>
2024-05-06 14:41:23 -0500

    Implementation of the messenger started

--- src/pages/dashboard/components/messenger/Messenger.jsx
+++ src/pages/dashboard/components/messenger/Messenger.jsx
@@ -2,21 +2,31 @@ import './Messenger.scss'
 import { socket } from '../../../../socket'
 import { useContext } from 'react'
 import { UserContext } from '../../../../App'
+import { useAuth0 } from '@auth0/auth0-react'
+import { MessengerCard } from './components/MessengerCard'
 
 export const Messenger = () => {
   const { activeParty } = useContext(UserContext)
-  console.log(activeParty)
+  const { user } = useAuth0()
+  const userId = user.sub.split('|')[1]
+
+  // TODO move these connection functions to a useEffect
   const connect = () => {
+    socket.auth = { userId: userId, partyId: activeParty.id }
     socket.connect()
   }
-
   const disconnect = () => {
     socket.disconnect()
   }
+
+  // TODO Implement useEffect function here
+  // On render it should get all the messages for the party
+  // and then connect to the socket.
+
   return (
     <>
       {activeParty && <p>{activeParty.id}</p>}
-      <p className="messenger">Messenger goes here!!!</p>
+      <MessengerCard />
       <button onClick={connect}>Connect</button>
       <button onClick={disconnect}>Disconnect</button>
     </>
---
+++ src/pages/dashboard/components/messenger/components/MessengerCard.jsx
@@ -0,0 +1,9 @@
+import './MessengerCard.scss'
+
+export const MessengerCard = () => {
+  return (
+    <>
+      <p>Messenger card</p>
+    </>
+  )
+}
