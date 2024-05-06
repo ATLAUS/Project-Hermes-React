0279bee96f5ae05592c8bc1854a1868b7915aa07
Aaron Sanchez <saarong12@gmail.com>
2024-05-06 17:31:15 -0500

    Initial fetch message function implemented.

--- src/pages/dashboard/components/messenger/Messenger.jsx
+++ src/pages/dashboard/components/messenger/Messenger.jsx
@@ -1,13 +1,14 @@
 import './Messenger.scss'
 import { socket } from '../../../../socket'
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { UserContext } from '../../../../App'
 import { useAuth0 } from '@auth0/auth0-react'
 import { MessengerCard } from './components/MessengerCard'
 
 export const Messenger = () => {
+  const [messages, setMessages] = useState(null)
   const { activeParty } = useContext(UserContext)
-  const { user } = useAuth0()
+  const { user, getAccessTokenSilently } = useAuth0()
   const userId = user.sub.split('|')[1]
 
   // TODO move these connection functions to a useEffect
@@ -19,16 +20,57 @@ export const Messenger = () => {
     socket.disconnect()
   }
 
+  // TODO Needs to go in the services file.
+  const fetchMessages = async () => {
+    try {
+      const accessToken = await getAccessTokenSilently({
+        authorizationParams: {
+          audience: 'http://localhost:3000/'
+        }
+      })
+
+      const customUserHeader = {
+        'X-User-Info': JSON.stringify({
+          email: user.email,
+          nickname: user.nickname,
+          sub: user.sub
+        })
+      }
+
+      const getMessages = `http://localhost:3000/api/messages/${activeParty.id}`
+
+      const getMessagesResponse = await fetch(getMessages, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+          ...customUserHeader
+        }
+      })
+
+      const response = await getMessagesResponse.json()
+
+      console.log(response)
+    } catch (e) {
+      console.log(`Error: ${e}`)
+    }
+  }
+
   // TODO Implement useEffect function here
   // On render it should get all the messages for the party
   // and then connect to the socket.
 
+  useEffect(() => {
+    activeParty && (connect(), fetchMessages())
+
+    return () => {
+      disconnect()
+    }
+  })
+
   return (
     <>
       {activeParty && <p>{activeParty.id}</p>}
-      <MessengerCard />
-      <button onClick={connect}>Connect</button>
-      <button onClick={disconnect}>Disconnect</button>
+      <MessengerCard messages={messages} />
     </>
   )
 }
