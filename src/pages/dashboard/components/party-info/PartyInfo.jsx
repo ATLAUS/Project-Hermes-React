3b9758c47960ae6582959731eff5c66f8a9ee4cb
danxguerrero <danxguerrero@yahoo.com>
2024-05-06 23:31:51 -0400

    Find MatchedUser and display their name on the page

--- src/pages/dashboard/components/party-info/PartyInfo.jsx
+++ src/pages/dashboard/components/party-info/PartyInfo.jsx
@@ -4,20 +4,37 @@ import LoopIcon from '@mui/icons-material/Loop';
 import PersonIcon from '@mui/icons-material/Person';
 import './PartyInfo.scss'
 // Import useContext and the context we created (UserContext)
-import { useContext, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { UserContext } from '../../../../App'
 
 export const PartyInfo = () => {
   // Destructure the parts of the context we are using
   const { userInfo, activeParty } = useContext(UserContext)
-  const [rematchin, setRematching] = useState(false)
+  const [rematching, setRematching] = useState(false)
   const [leavingParty, setLeavingParty] = useState(false)
+  const [matchedUser, setMatchedUser] = useState({})
 
-  console.log(userInfo)
+  const findMatchedUser = () => {
+    if (activeParty) {
+      const users = activeParty?.Users
+      console.log(users)
+      for (const user of users) {
+        if (user !== userInfo) {
+          setMatchedUser(user)
+        }
+      }
+    }
+  }
+
+  useEffect(() => {
+    findMatchedUser()
+  }, [activeParty])
+
+
   return (
     <Box className="party-info-container">
       <Stack spacing={15} sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-        <Typography>{userInfo?.userName}</Typography>
+        <Typography>{matchedUser?.userName}</Typography>
         <PersonIcon fontSize='large' />
         <Stack direction="row" spacing={7}>
           <Button><LogoutIcon/></Button>
