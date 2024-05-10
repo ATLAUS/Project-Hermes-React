6a47313817a28f7add6b7cbbe70afdadb3667f8d
ghezaii <ghezai@google.com>
2024-05-10 12:01:43 -0400

    restyled matcher form and matcher info boxes

--- src/pages/matcher-display/components/matcher-info/MatcherInfo.jsx
+++ src/pages/matcher-display/components/matcher-info/MatcherInfo.jsx
@@ -1,24 +1,36 @@
 import { Box, Typography, Stack } from '@mui/material'
+import SportsEsportsOutlinedIcon from '@mui/icons-material/SportsEsportsOutlined';
+import FlagCircleOutlinedIcon from '@mui/icons-material/FlagCircleOutlined';
+import NoteAddOutlinedIcon from '@mui/icons-material/NoteAddOutlined';
 import './MatcherInfo.scss'
 
 export const MatcherInfo = () => {
     return(
         <Box className = 'matcher-info-container'>
             <Box className = 'matcher-info-box'>
-                <Stack>
-                    <Typography>Choose a Game</Typography>
-                    <Typography className='text-specifications'>Lorem ipsum dolor sit amet, consectetur adipiscing elit</Typography>
+                <Stack direction="column" spacing={2}>
+                    <Stack className = 'matcher-info-text' direction = "row" spacing = {2}>
+                        <SportsEsportsOutlinedIcon fontSize = "large" />
+                        <Typography>Choose a Game</Typography>
+                    </Stack>
+                    <Typography className='text-specifications'>Select a game to play from our list of ... pre-determined titles.</Typography>
                 </Stack>
             </Box>
             <Box className = 'matcher-info-box'>
-                 <Stack>
-                    <Typography>Select an Objective</Typography>
-                    <Typography className='text-specifications'>Lorem ipsum dolor sit amet, consectetur adipiscing elit</Typography>
+                 <Stack direction="column" spacing={2}>
+                    <Stack className='matcher-info-text' direction="row" spacing = {2}>
+                        <FlagCircleOutlinedIcon fontSize='large'/>
+                        <Typography>Select an Objective</Typography>
+                    </Stack>
+                    <Typography className='text-specifications'>Set a clear and concise objective that gives your teammate direction regarding what you expect to accomplish with them in game. </Typography>
                 </Stack>
             </Box>
             <Box className = 'matcher-info-box'>
-                 <Stack>
-                    <Typography>Add a Note</Typography>
+                 <Stack direction="column" spacing={2}>
+                    <Stack className='matcher-info-text' direction="row" spacing = {2}>
+                        <NoteAddOutlinedIcon fontSize='large' />
+                        <Typography>Add a Note</Typography>
+                    </Stack>
                     <Typography className='text-specifications'>Lorem ipsum dolor sit amet, consectetur adipiscing elit</Typography>
                 </Stack>
             </Box>
